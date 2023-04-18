b7640247643cbc70ea68c0f99f89e42a53280a3c
NF971132 <66896359+NF971132@users.noreply.github.com>
2023-04-18 20:12:46 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("华文楷体", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(239, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 68);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "自动分组";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(97, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "输入人数";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "输入空号";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(205, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "输入带头人";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(203, 156);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 28);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(16, 156);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 28);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(585, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 44);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "完成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(309, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 38);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "下一个";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(122, 155);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 40);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "下一个";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(16, 190);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(100, 256);
+            this.listBox1.TabIndex = 10;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 18;
+            this.listBox2.Location = new System.Drawing.Point(203, 190);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(100, 256);
+            this.listBox2.TabIndex = 11;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 18;
+            this.listBox3.Location = new System.Drawing.Point(494, 157);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(303, 292);
+            this.listBox3.TabIndex = 12;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 18;
+            this.listBox4.Location = new System.Drawing.Point(390, 157);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(98, 292);
+            this.listBox4.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(501, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 18);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "输出结果";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(396, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 18);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "可用座号";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(122, 201);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 38);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "撤回";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(309, 201);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 38);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "撤回";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(309, 407);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 39);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "去重";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(703, 107);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(85, 44);
+            this.button7.TabIndex = 19;
+            this.button7.Text = "输出为文本";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static void RandomArr(int[] arr)
+        {
+            Random r = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int index = r.Next(arr.Length);
+                int temp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = temp;
+            }
+        }
+        public void yxti1()
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                for (int j = 0; j < listBox4.Items.Count; j++)
+                {
+                    if (listBox4.Items[j].Equals(listBox1.Items[i]))
+                        listBox4.Items.RemoveAt(j);
+                }
+            }
+        }
+        public void yxti2()
+        {
+            for (int i = 0; i < listBox2.Items.Count; i++)
+            {
+                for (int j = 0; j < listBox4.Items.Count; j++)
+                {
+                    if (listBox4.Items[j].Equals(listBox2.Items[i]))
+                    {
+                        listBox4.Items.RemoveAt(j);
+                    }
+                }
+            }
+        }
+        public void quis()
+        {
+            for (int i = 0; i < listBox2.Items.Count; i++)
+            {
+                for (int j = 0; j < listBox1.Items.Count; j++)
+                {
+                    if (listBox2.Items[i].Equals(listBox1.Items[j]))
+                    {
+                        listBox1.Items.RemoveAt(j);
+                    }
+                }
+            }
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                for (int j = 0; j < listBox1.Items.Count; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    else if (listBox1.Items[i].Equals(listBox1.Items[j]))
+                    {
+                        listBox1.Items.RemoveAt(i);
+                    }
+                }
+            }
+            for (int i = 0; i < listBox2.Items.Count; i++)
+            {
+                for (int j = 0; j < listBox2.Items.Count; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    else if (listBox2.Items[i].Equals(listBox2.Items[j]))
+                    {
+                        listBox2.Items.RemoveAt(i);
+                    }
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8)//允许输入数字和退格
+                e.Handled = true;
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8)//允许输入数字和退格
+                e.Handled = true;
+            if (textBox1.Text == "")
+                e.Handled = true;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8)//允许输入数字和退格
+                e.Handled = true;
+            if (textBox1.Text == "")
+                e.Handled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "")
+            {
+                button3.Enabled = false;
+            }
+            else
+            {
+                button3.Enabled = true;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                button2.Enabled = false;
+            }
+            else
+            {
+                button2.Enabled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "0")
+            {
+                MessageBox.Show("座号不能为零");
+                textBox3.Clear();
+            }
+            else if (Convert.ToInt32(textBox3.Text) > Convert.ToInt32(textBox1.Text))
+            {
+                MessageBox.Show("座号超出范围");
+                textBox3.Clear();
+            }
+            else if (listBox1.Items.Count > Convert.ToInt32(textBox1.Text) - 1)
+            {
+                MessageBox.Show("人数太多了");
+                textBox3.Clear();
+            }
+            else
+            {
+                listBox1.Items.Add(textBox3.Text);
+                textBox3.Clear();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0")
+            {
+                MessageBox.Show("带头人不能为零");
+                textBox2.Clear();
+            }
+            else if (Convert.ToInt32(textBox2.Text) > Convert.ToInt32(textBox1.Text))
+            {
+                MessageBox.Show("座号超出范围");
+                textBox2.Clear();
+            }
+            else if (listBox2.Items.Count > Convert.ToInt32(textBox1.Text) - 1)
+            {
+                MessageBox.Show("人数太多了");
+                textBox2.Clear();
+            }
+            else
+            {
+                listBox2.Items.Add(textBox2.Text);
+                textBox2.Clear();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count == 0)
+            {
+                MessageBox.Show("没法再撤了(＞︿＜)");
+            }
+            else
+            {
+                listBox1.Items.RemoveAt(listBox1.Items.Count - 1);
+                textBox3.Clear();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listBox2.Items.Count == 0)
+            {
+                MessageBox.Show("没法再撤了(＞︿＜)");
+            }
+            else
+            {
+                listBox2.Items.RemoveAt(listBox2.Items.Count - 1);
+                textBox2.Clear();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox2.Items.Count == 0)
+            {
+                MessageBox.Show("带头人至少输入一个");
+            }
+            else
+            {
+                listBox3.Items.Clear();
+                if (listBox4.Items.Count > 0)
+                {
+                    listBox4.Items.Clear();
+                }
+                quis();
+                if (textBox1.Text == "" || textBox1.Text == "0")
+                {
+                    MessageBox.Show("人数不能为空或为零");
+                    textBox1.Clear();
+                }
+                for (int i = 0; i < Convert.ToInt32(textBox1.Text); i++)
+                {
+                    listBox4.Items.Add(Convert.ToString(i + 1));
+                }
+                yxti2();
+                yxti1();
+
+                int arlong = listBox4.Items.Count / listBox2.Items.Count;
+                int arlongy = listBox4.Items.Count % listBox2.Items.Count;
+
+                int[] rfuu = new int[listBox4.Items.Count];
+                int ptr = 0;
+                for (int i = 0; i < listBox4.Items.Count; i++)
+                {
+                    rfuu[i] = Convert.ToInt32(listBox4.Items[i]);
+                }
+                RandomArr(rfuu);
+                List<string> listzh = new List<string>();
+                for (int i = 0; i < listBox2.Items.Count; i++)
+                {
+                    string jle = "";
+                    for (int j = ptr; j < ptr + arlong; j++)
+                    {
+                        jle += rfuu[j].ToString() + ",";
+                    }
+                    jle = jle.Substring(0, jle.Length - 1);
+                    ptr += arlong;
+                    listzh.Add("第" + (i + 1) + "组—" + listBox2.Items[i].ToString() + " : " + jle);
+                }
+                foreach (string s in listzh)
+                {
+                    listBox3.Items.Add(s);
+                }
+                string doyu = "剩余的座号：";
+                for (int i = ptr; i < rfuu.Length; i++)//
+                {
+                    doyu += rfuu[i].ToString() + ",";
+                }
+                doyu = doyu.Substring(0, doyu.Length - 1);
+                listBox3.Items.Add(doyu);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            quis();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (listBox3.Items.Count != 0)
+            {
+                string[] str = new string[listBox3.Items.Count];
+                for (int i = 0; i < listBox3.Items.Count; i++)
+                {
+                    str[i] = listBox3.Items[i].ToString();
+                }
+                File.WriteAllLines(@".\分组.txt", str);
+                MessageBox.Show("创建完成");
+            }
+            else
+            {
+                MessageBox.Show("无内容");
+            }
+        }
+    }
+}
